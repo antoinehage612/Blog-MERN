6dc895ebb8dead33b656a8eb81072d57e0ac3ed3
antoinehage612 <hypercoffin612@gmail.com>
2024-02-19 18:08:23 +0200

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,19 @@
+import "./index.css";
+import App from "./App.jsx";
+import { store, persistor } from "./redux/store.js";
+import ThemeProvider from "./components/ThemeProvider.jsx";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </PersistGate>
+);
