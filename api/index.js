99f115562a3172766e4ca6df132f8a468500bbf1
antoinehage612 <hypercoffin612@gmail.com>
2024-02-23 12:41:31 +0200

    dashProfile full functionality

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
@@ -19,6 +20,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000!");
